6392be8ef1b2817a211c3e53f3e5c3d925a78cd9
João Francisco Lattaro Guerreiro <lattaro@tuta.io>
2026-05-29 00:45:38 -0300

    ci: Corrigida indentação do projeto

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,9 @@
-import Header from "@/components/header";
+import Header from '@/components/header'
 
 export default function Home() {
   return (
     <div className="">
-        <Header/>
+      <Header />
     </div>
-  );
+  )
 }
--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,3 @@
 export default function Header() {
-  return (
-    <div>
-        
-    </div>
-  );
+  return <div></div>
 }
