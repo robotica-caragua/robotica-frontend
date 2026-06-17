0257cdb679722015773ff41efe74b9a7ab9516dd
Ana Laura <analaurafiladelpho1110@gmail.com>
2026-06-17 20:24:43 -0300

    finalizada página de login

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -2,6 +2,8 @@ import { Button } from '@/components/Button'
 import { Header } from '@/components/Header'
 import Image from 'next/image'
 import Link from 'next/link'
+import boblogin from '@/assets/boblogin.png'
+import google from '@/assets/google.png'
 
 export default function LoginPage() {
   return (
@@ -9,58 +11,68 @@ export default function LoginPage() {
       <Header />
       <div>
         <div className="flex flex-row gap-2 justify-center">
-          <h1 className="font-bold text-4xl text-verde-escuro">ACESSE SUA</h1>
-          <h1 className="font-bold text-4xl text-verde-musgo">CONTA!</h1>
+          <h1 className="font-bold text-4xl text-verde-escuro">
+            ACESSE SUA <span className="text-verde-musgo">CONTA!</span>
+          </h1>
         </div>
 
-        <div className="flex flex-row">
+        <div className="mx-auto flex flex-row bg-white w-5xl rounded-lg border-2 border-verde-musgo my-10">
           <div>
-            <Image
-              className=""
-              src="https://placehold.co/540x540.png"
-              alt=""
-              width={540}
-              height={540}
-              loading="lazy"
-            />
+            <Image src={boblogin} alt="Robô BioBot" width={540} height={540} loading="eager" />
           </div>
 
           <div>
-            <form>
-              <label htmlFor="email-field">E-mail</label>
-              <input type="email" id="email-field" placeholder="exemplo@gmail.com" />
-              <label htmlFor="password-field">Senha</label>
-              <input type="password" id="password-field" placeholder="Digite sua senha..." />
-              <Link href="/">Esqueceu sua senha?</Link>
+            <form className="flex flex-col">
+              <label
+                htmlFor="email-field"
+                className="mt-22 mb-2 text-xl text-verde-escuro font-medium"
+              >
+                E-mail
+              </label>
+              <input
+                className="mb-5 h-10 bg-verde-pastel rounded-md border border-verde-musgo px-3"
+                type="email"
+                id="email-field"
+                placeholder="exemplo@gmail.com"
+              />
+              <label
+                htmlFor="password-field"
+                className="mb-2 text-xl text-verde-escuro font-medium"
+              >
+                Senha
+              </label>
+              <input
+                className="h-10 bg-verde-pastel rounded-md border border-verde-musgo px-3"
+                type="password"
+                id="password-field"
+                placeholder="Digite sua senha..."
+              />
+              <Link
+                href="/"
+                className="mt-2 text-sm text-verde-musgo underline hover:brightness-50 duration-75"
+              >
+                Esqueceu sua senha?
+              </Link>
 
-              <Button />
+              <Button>Entrar</Button>
             </form>
 
-            <div>
-              <div className="flex flex-row">
-                <div className="flex items-center gap-4 w-[370px] mt-8">
-                  <div className="h-px flex-1 bg-gray-600"></div>
-                </div>
+            <div className="flex items-center gap-4 w-92 mt-8">
+              <div className="h-px flex-1 bg-gray-600"></div>
 
-                <span>OU</span>
+              <span>OU</span>
 
-                <div className="flex items-center gap-4 w-[370px] mt-8">
-                  <div className="h-px flex-1 bg-gray-600"></div>
-                </div>
-              </div>
+              <div className="h-px flex-1 bg-gray-600"></div>
+            </div>
 
-              <div>
-                <button type="button">
-                  <Image
-                    src="https://placehold.co/18x18.png"
-                    alt=""
-                    width={18}
-                    height={18}
-                    loading="lazy"
-                  />
-                  <span>CONTINUAR COM O GOOGLE</span>
-                </button>
-              </div>
+            <div className="mt-5 flex justify-center">
+              <button
+                type="button"
+                className="flex items-center gap-3 h-8 w-56 px-3 bg-verde-pastel rounded-md font-medium text-xs text-verde-escuro"
+              >
+                <Image src={google} alt="Logo da Google" width={18} height={18} loading="eager" />
+                <a className="cursor-pointer">CONTINUAR COM O GOOGLE</a>
+              </button>
             </div>
           </div>
         </div>
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,10 @@
-export function Button(){
-    return(
-        <button type="submit">
-            <span>
-                Entrar
-            </span>
-        </button>
-    )
+export function Button({ children }) {
+  return (
+    <button
+      type="submit"
+      className="mt-10 mx-auto flex w-35 h-10 items-center justify-center gap-2 rounded-xl bg-verde-limao border-b-6 border-verde-escuro font-bold text-verde-escuro hover:brightness-50 duration-75 cursor-pointer"
+    >
+      <span>{children}</span>
+    </button>
+  )
 }
